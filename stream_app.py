96321bdb3e8412bb1ceb7737b930d294a9057ce2
Simo Defo Fabrice <fabricesimodefo@gmail.com>
2022-07-19 09:51:36 -0400

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -7,6 +7,16 @@ import constraint_P
 import streamlit as st
 import base64
 import plotly.graph_objects as go
+import jdk
+
+jdk.install('11')
+# Platform dependent install of Java JDK 11 into $HOME/.jdk/<VERSION>
+
+jdk.install('11', impl=jdk.Implementation.OPENJ9)
+# Platform dependent install of Java JDK 11 with OpenJ9 into $HOME/.jdk/<VERSION>
+
+jdk.install('11', jre=True)
+# Platform dependent install of Java JRE 11 into $HOME/.jre/<VERSION>
 
 path = '/home/ff/Documents/UDepLambdaaIrudi'
 
